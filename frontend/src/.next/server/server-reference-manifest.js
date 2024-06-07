1bbc3b0a67161a9173555d2e351a308afa8b1e39
Raphael Oliveira <116489080+Oliveiraster@users.noreply.github.com>
2024-06-07 15:17:46 -0300

    Este Pull Request reestrutura o projeto para utilizar JavaScript (JS) em vez de TypeScript (TS)

--- frontend/src/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"vq4Dbowx7llKNx2Ni23gM+DhzDgH7fMYEkWCSfKCgJQ=\"\n}"
---
+++ frontend/src/components/Header/index.js
@@ -0,0 +1,9 @@
+import style from './header.module.css'
+
+export default function Header() {
+  return (
+    <nav className={style.container}> 
+      Header
+    </nav>
+  );
+}

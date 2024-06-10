b6a682c868de4ed7f9d038148381a3076c330c9f
Wesley Gomes <wesleywesleygomesgomes@gmail.com>
2024-06-10 17:53:36 -0300

    feat: faz o header responsivo

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -2,28 +2,45 @@
 import style from "./header.module.css";
 import Link from "next/link";
 import ProfileIcon from "../ProfileIcon";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Button from "../Button";
+import Menu from "../Menu";
 
 export default function Header() {
   const [isLogged, setIsLogged] = useState(true)
+  const [windowWidth, setWindowWidth] = useState(0)
+  useEffect(() => {
+    setWindowWidth(window.innerWidth)
+  }, [])
+
+
   return (
     <header className={style.container}>
       <Link href="/" className={style.logo}>Libs</Link>
       {isLogged ?
         <nav className={style.nav}>
-          <Link className={style.navItem} href="Livros">
-            Livros
-          </Link>
-          <Link className={style.navItem} href="Salvos">
-            Salvos
-          </Link>
-          <Link className={style.navItem} href="Blogs">
-            Blogs
-          </Link>
-          <div className={style.icon}>
-            <ProfileIcon />
-          </div>
+          {windowWidth < 425 ?
+            <>
+              <Link className={style.navItem} href="Blogs">
+                Blogs
+              </Link>
+              <ProfileIcon />
+              <Menu />
+            </>
+            :
+            <>
+              <Link className={style.navItem} href="Livros">
+                Livros
+              </Link>
+              <Link className={style.navItem} href="Salvos">
+                Salvos
+              </Link>
+              <Link className={style.navItem} href="Blogs">
+                Blogs
+              </Link>
+              <ProfileIcon />
+            </>
+          }
         </nav>
         :
         <nav className={style.nav}>

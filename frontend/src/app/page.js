a5e7407a40099443b8b77a8a6cfdaac04097b731
Wesley Gomes <wesleywesleygomesgomes@gmail.com>
2024-06-21 14:40:42 -0300

    refactor: Faz o cardBook usar o container como wrapper

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,6 +1,7 @@
 import CardBlog from "@/components/CardBlog";
 import Header from "@/components/Header";
 import style from "./page.module.css"
+import CardBook from "@/components/CardBook";
 
 export default function Home() {
   return (
@@ -8,6 +9,9 @@ export default function Home() {
       <Header />
       <div className={style.centralize}>
         <CardBlog />
+      </div>
+      <div className={style.centralize}>
+        <CardBook />
       </div>
     </main>
   );
--- frontend/src/components/CardBook/index.js
+++ frontend/src/components/CardBook/index.js
@@ -3,12 +3,13 @@ import ProfileIcon from '../ProfileIcon';
 import style from './card.module.css';
 import saved from "/src/assets/saved.png"
 import message from "/src/assets/message.png"
+import Container from '../Container';
 export default function CardBook() {
-
+    const books = [1, 2, 3, 4, 5, 6, 7, 8]
     return (
-        <section className={style.container}>
-            <div className={style.card}>
-                <div className={style.containerMenor}>
+        <Container>
+            <div className={style.container}>
+                {books.map((item, id) => (
                     <div className={style.cardBook}>
                         <div className={style.titulos}>
                             <ProfileIcon />
@@ -16,110 +17,16 @@ export default function CardBook() {
                         </div>
                         <img className={style.book} src="#" alt="icon" />
                         <div className={style.infos}>
-                            <Button p={2} width={17} variant="green" icon={saved} />
-                            <Button p={2} width={20} variant="green" icon={message} />
-                            <Button text="Pedir" variant="blue" e textColor="--base-white" />
+                            <Button p="0" width={14} variant="green" icon={saved} />
+                            <Button p="0" width={15} variant="green" icon={message} />
+                            <Button text="pedir" p="1px 14px" variant="blue" textColor="--base-white" />
                         </div>
                     </div>
-
-                    <div className={style.cardBook}>
-                        <div className={style.titulos}>
-                            <ProfileIcon />
-                            <h3 className={style.autor}>Nome do autor</h3>
-                        </div>
-                        <img className={style.book} src="#" alt="icon" />
-                        <div className={style.infos}>
-                            <Button p={2} width={17} variant="green" icon={saved} />
-                            <Button p={2} width={20} variant="green" icon={message} />
-                            <Button text="Pedir" variant="blue" e textColor="--base-white" />
-                        </div>
-                    </div>
-
-                    <div className={style.cardBook}>
-                        <div className={style.titulos}>
-                            <ProfileIcon />
-                            <h3 className={style.autor}>Nome do autor</h3>
-                        </div>
-                        <img className={style.book} src="#" alt="icon" />
-                        <div className={style.infos}>
-                            <Button p={2} width={17} variant="green" icon={saved} />
-                            <Button p={2} width={20} variant="green" icon={message} />
-                            <Button text="Pedir" variant="blue" e textColor="--base-white" />
-                        </div>
-                    </div>
-
-                    <div className={style.cardBook}>
-                        <div className={style.titulos}>
-                            <ProfileIcon />
-                            <h3 className={style.autor}>Nome do autor</h3>
-                        </div>
-                        <img className={style.book} src="#" alt="icon" />
-                        <div className={style.infos}>
-                            <Button p={2} width={17} variant="green" icon={saved} />
-                            <Button p={2} width={20} variant="green" icon={message} />
-                            <Button text="Pedir" variant="blue" e textColor="--base-white" />
-                        </div>
-                    </div>
-
-                    <div className={style.cardBook}>
-                        <div className={style.titulos}>
-                            <ProfileIcon />
-                            <h3 className={style.autor}>Nome do autor</h3>
-                        </div>
-                        <img className={style.book} src="#" alt="icon" />
-                        <div className={style.infos}>
-                            <Button p={2} width={17} variant="green" icon={saved} />
-                            <Button p={2} width={20} variant="green" icon={message} />
-                            <Button text="Pedir" variant="blue" e textColor="--base-white" />
-                        </div>
-                    </div>
-
-                    <div className={style.cardBook}>
-                        <div className={style.titulos}>
-                            <ProfileIcon />
-                            <h3 className={style.autor}>Nome do autor</h3>
-                        </div>
-                        <img className={style.book} src="#" alt="icon" />
-                        <div className={style.infos}>
-                            <Button p={2} width={17} variant="green" icon={saved} />
-                            <Button p={2} width={20} variant="green" icon={message} />
-                            <Button text="Pedir" variant="blue" e textColor="--base-white" />
-                        </div>
-                    </div>
-
-                    <div className={style.cardBook}>
-                        <div className={style.titulos}>
-                            <ProfileIcon />
-                            <h3 className={style.autor}>Nome do autor</h3>
-                        </div>
-                        <img className={style.book} src="#" alt="icon" />
-                        <div className={style.infos}>
-                            <Button p={2} width={17} variant="green" icon={saved} />
-                            <Button p={2} width={20} variant="green" icon={message} />
-                            <Button text="Pedir" variant="blue" e textColor="--base-white" />
-
-                        </div>
-                    </div>
-
-                    <div className={style.cardBook}>
-                        <div className={style.titulos}>
-                            <ProfileIcon />
-                            <h3 className={style.autor}>Nome do autor</h3>
-                        </div>
-                        <img className={style.book} src="#" alt="icon" />
-                        <div className={style.infos}>
-                            <Button p={2} width={17} variant="green" icon={saved} />
-                            <Button p={2} width={20} variant="green" icon={message} />
-                            <Button text="Pedir" variant="blue" e textColor="--base-white" />
-                        </div>
-                    </div>
-
-                    <div className={style.div}>
-                        <Button text="Ver todos os livros" variant="blue" e textColor="--base-white" />
-                    </div>
-
+                ))}
+                <div className={style.div}>
+                    <Button text="Ver todos os livros" variant="blue" textColor="--base-white" />
                 </div>
             </div>
-        </section>
+        </Container>
     )
 }
